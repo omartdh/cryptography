977c7d0f751cd6f4478c085a7995848e0d0cf7bb
Omar Dahmash <omartdh@omars-mbp.home>
2021-05-15 11:45:10 -0700

    modifeied api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-token = "5d0bdc15-50d5-4ca0-853d-1a63ca90efaa"
+// token = "5d0bdc15-50d5-4ca0-853d-1a63ca90efaa"
 
-axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
- headers: {
-   'X-CMC_PRO_API_KEY': token, 
- } //the token is a variable which holds the token
- }).then(function(req)  {
-  //  console.log(req);
-  console.log(req.data.data);
- console.log(req.data.data[0].quote.USD.price);
-})
+// axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+//  headers: {
+//    'X-CMC_PRO_API_KEY': token, 
+//  } //the token is a variable which holds the token
+//  }).then(function(req)  {
+//   //  console.log(req);
+//   console.log(req.data.data);
+//  console.log(req.data.data[0].quote.USD.price);
+// })
 
 export default {
   
